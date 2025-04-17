499404165d33ad3de7a56d7d6642e0fdb5410d1a
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-17 15:59:13 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -18,6 +18,7 @@ show_menu() {
   echo -e "  ${yellow}❼${reset} ${red}卸载${reset}           ${yellow}⓿${reset} ${red}退出${reset}"
   echo -e "${blue}╚═════════════════════════════════════════════════════════════════════════════════╝${reset}"
 }
+while true; do
   show_menu
   read -p "请输入选项编号： " opt
   case "$opt" in
@@ -33,12 +34,9 @@ show_menu() {
        sleep 0.5
        exit 0 ;;
     *) 
-      if [[ "$opt" =~ ^[1-7]$ ]]; then
-        bash "/root/VPN/menu/${options[$opt]}.sh"
-      else
-        echo -e "${red}❌ 无效输入！${reset}"
+      echo -e "${red}❌ 无效输入！${reset}"
         sleep 0.5
-      fi
       ;;
   esac
+done
 chmod +x menu.sh
