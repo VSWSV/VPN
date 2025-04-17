025d89801d4bb72110fd4998de0b0607ee16d652
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-17 15:34:53 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -29,7 +29,7 @@ while true; do
     5) bash /root/VPN/menu/maintain.sh ;;
     6) bash /root/VPN/menu/network_tools.sh ;;
     7) bash /root/VPN/menu/uninstall.sh ;;
-    0) echo -e "正在退出..."
+    0) echo -e "${green}正在退出${reset}"
        sleep 0.5
        exit 0
       ;;
