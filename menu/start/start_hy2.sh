39239fcc9fa0b41e19b1762bf64eb6371bb3505d
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-20 06:18:25 +0800

    Update start_hy2.sh

--- menu/start/start_hy2.sh
+++ menu/start/start_hy2.sh
@@ -66,7 +66,7 @@ fi
 
 # 启动服务
 echo -e "${yellow}🔄 正在启动服务...${reset}"
-nohup /root/VPN/hysteria/hysteria server --config "$CONFIG_PATH" > "$LOG_PATH" 2>&1 &
+nohup /root/VPN/hysteria server --config "$CONFIG_PATH" > "$LOG_PATH" 2>&1 &
 echo $! > "$PID_PATH"
 sleep 1
 
