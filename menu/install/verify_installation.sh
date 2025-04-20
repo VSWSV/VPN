35a48ad49d075b4556ddebf517cbb53e6c031461
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-20 18:31:38 +0800

    Update verify_installation.sh

--- menu/install/verify_installation.sh
+++ menu/install/verify_installation.sh
@@ -37,7 +37,7 @@ echo -e "${cyan}╠════════════════════�
 # 1. 验证组件版本
 components=(
   "Xray|xray/xray version|Xray"
-  "Hysteria|/root/VPN/hysteria version|version:"
+  "Hysteria|/root/VPN/hysteria version|Version:"
   "Cloudflared|cloudflared --version|cloudflared"
 )
 
