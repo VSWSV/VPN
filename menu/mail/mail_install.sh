2d3428f39dc79f80e81a4f9fbd3809e2edbbce2d
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-27 03:27:11 +0800

    Update mail_install.sh

--- menu/mail/mail_install.sh
+++ menu/mail/mail_install.sh
@@ -13,9 +13,9 @@ reset="\033[0m"
 
 # 密码确认
 echo -e "${yellow}⚡ 安装操作需要输入密码确认${reset}"
-read -p "请输入密码以继续（默认密码: 888）: " user_pass
+read -p "请输入密码以继续（默认密码: 88）: " user_pass
 
-if [ "$user_pass" != "888" ]; then
+if [ "$user_pass" != "88" ]; then
   echo -e "${red}❌ 密码错误，安装已取消，返回菜单！${reset}"
   read -p "$(echo -e "💬 ${cyan}按回车键返回...${reset}")" dummy
   bash /root/VPN/menu/mail.sh
