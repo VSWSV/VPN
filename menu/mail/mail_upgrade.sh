76145a43ee5914762a6385c94e89df15de039f4e
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-27 03:16:36 +0800

    Update mail_upgrade.sh

--- menu/mail/mail_upgrade.sh
+++ menu/mail/mail_upgrade.sh
@@ -12,9 +12,9 @@ reset="\033[0m"
 
 # 密码确认
 echo -e "${yellow}⚡ 卸载操作非常危险，需要输入密码确认${reset}"
-read -p "请输入密码以继续（默认密码: 888）: " user_pass
+read -p "请输入密码以继续（默认密码: 88）: " user_pass
 
-if [ "$user_pass" != "888" ]; then
+if [ "$user_pass" != "88" ]; then
   echo -e "${red}❌ 密码错误，卸载已取消！${reset}"
   sleep 0.5
   bash /root/VPN/menu/mail.sh
