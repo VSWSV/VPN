1e0136ea1ede4b6296b3c8c2f1ab60033ea509b5
VSWSV <154340263+VSWSV@users.noreply.github.com>
2025-04-29 13:11:01 +0800

    Update mail.sh

--- menu/mail.sh
+++ menu/mail.sh
@@ -15,9 +15,9 @@ show_menu() {
     echo -e "${pink}╔═════════════════════════════════════════════════════════════════════════════════╗${reset}"
     echo -e "                                  ${orange}✉️ 邮局-系统${reset}"
     echo -e "${pink}╠═════════════════════════════════════════════════════════════════════════════════╣${reset}"
-    echo -e "  ${yellow}❶${reset} ${green}邮局系统安装${reset}                   ${yellow}❷${reset} ${green}邮局系统升级${reset}                  ${yellow}❸${reset} ${green}服务启停状态${reset}"
-    echo -e "  ${yellow}❹${reset} ${green}邮局域名配置${reset}                   ${yellow}❺${reset} ${green}邮局账户管理${reset}                  ${yellow}❻${reset} ${green}邮局系统测试${reset}"
-    echo -e "  ${yellow}⓿${reset} ${red}返回主菜单${reset}"
+    echo -e "  ${yellow}❶${reset} ${green}邮局系统安装${reset}                   ${yellow}❷${reset} ${green}邮局域名配置${reset}                  ${yellow}❸${reset} ${green}邮局账户管理${reset}"
+    echo -e "  ${yellow}❹${reset} ${green}服务启停状态${reset}                   ${yellow}❺${reset} ${green}数据库控制板${reset}                  ${yellow}❻${reset} ${green}邮局系统测试${reset}"
+    echo -e "  ${yellow}❼${reset} ${red}邮局系统卸载${reset}                     ${yellow}⓿${reset} ${red}返回主菜单${reset}"
     echo -e "${pink}╚═════════════════════════════════════════════════════════════════════════════════╝${reset}"
 }
 
@@ -27,15 +27,17 @@ while true; do
     case $opt in
         1) bash /root/VPN/menu/mail/mail_install.sh
             break ;;
-        2) bash /root/VPN/menu/mail/mail_upgrade.sh
+        2) bash /root/VPN/menu/mail/mail_configure.sh
             break ;;
-        3) bash /root/VPN/menu/mail/mail_khiav.sh
+        3) bash /root/VPN/menu/mail/mail_Account.sh
            break ;;
-        4) bash /root/VPN/menu/mail/mail_configure.sh
+        4) bash /root/VPN/menu/mail/mail_khiav.sh
             break ;;
-        5) bash /root/VPN/menu/mail/mail_account.sh
+        5) bash /root/VPN/menu/mail/mail_database.sh
             break ;;
         6) bash /root/VPN/menu/mail/mail_test.sh
+            break ;;
+        7) bash /root/VPN/menu/mail/mail_uninstall.sh
             break ;;
         0) bash /root/VPN/menu.sh
             break ;;
